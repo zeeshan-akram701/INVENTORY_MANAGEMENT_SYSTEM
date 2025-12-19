99a881920c66565177ec68bec356963a32fb8fc2
zeeshanakramla1122 <zeeshanakramla1122@gmail.com>
2025-12-19 18:47:07 +0500

    Add project files.

---
+++ INVENTORY_MANAGEMENT_SYSTEM/Form1.cs
@@ -0,0 +1,15 @@
+namespace INVENTORY_MANAGEMENT_SYSTEM
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
